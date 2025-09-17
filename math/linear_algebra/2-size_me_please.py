7a4219c7236dc0d166c5d272cf99ebc3be661f68
Elvis Kerebi <kerebipreye@gmail.com>
2025-09-17 14:30:07 +0200

    added script 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Matrix shape utility."""
+
+
+def matrix_shape(matrix):
+    """Return the shape of a nested list (matrix) as a list of ints."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if not matrix:
+            break
+        matrix = matrix[0]
+    return shape
+
+
