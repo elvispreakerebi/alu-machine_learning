d2699765672d45e31e099bb1c41596efe5abcf8b
Elvis Kerebi <kerebipreye@gmail.com>
2025-10-26 11:48:06 +0200

    fixed 1 consolve

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -2,6 +2,7 @@
 """Same convolution for grayscale images with two for loops max."""
 import numpy as np
 
+
 def convolve_grayscale_same(images, kernel):
     """Performs a same convolution on grayscale images with zero-padding.
 
@@ -15,9 +16,11 @@ def convolve_grayscale_same(images, kernel):
     kh, kw = kernel.shape
     pad_h = (kh - 1) // 2 if kh % 2 == 1 else kh // 2
     pad_w = (kw - 1) // 2 if kw % 2 == 1 else kw // 2
-    images_padded = np.pad(images,
-                          ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                          mode='constant')
+    images_padded = np.pad(
+        images,
+        ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+        mode='constant'
+    )
     conv = np.zeros((m, h, w))
     for i in range(h):
         for j in range(w):
