21d6117629825b47ac8d7469baf2645895951147
Elvis Preye Kerebi <e.kerebi@alustudent.com>
2026-05-06 12:04:37 +0200

    fixed 5

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -29,7 +29,7 @@ class NST:
             beta (float): Weight for style cost.
 
         After init, sets style_image, content_image, alpha, beta, model,
-        gram_style_features, and content_feature.
+        style_features, gram_style_features, and content_feature.
         """
         if (not isinstance(style_image, np.ndarray) or
                 style_image.ndim != 3 or style_image.shape[2] != 3):
@@ -61,32 +61,66 @@ class NST:
 
         Builds a Keras model from VGG19 (no top) with the same input as VGG19
         and outputs the style layer activations followed by the content layer.
+        Max-pooling layers are replaced by average pooling (NST convention).
+        Layer outputs are taken from the rebuilt forward path (not
+        get_layer().output on reused layers, which can reference the old
+        VGG graph).
         """
+        style_names = list(self.style_layers)
+        content_name = self.content_layer
+
         vgg = tf.keras.applications.VGG19(
             include_top=False,
             weights='imagenet',
         )
         vgg.trainable = False
-        outputs = (
-            [vgg.get_layer(name).output for name in self.style_layers] +
-            [vgg.get_layer(self.content_layer).output]
+        x = vgg.input
+        tensors_by_layer = {}
+        for layer in vgg.layers[1:]:
+            if isinstance(layer, tf.keras.layers.MaxPooling2D):
+                x = tf.keras.layers.AveragePooling2D(
+                    pool_size=layer.pool_size,
+                    strides=layer.strides,
+                    padding=layer.padding,
+                    name=layer.name,
+                )(x)
+            else:
+                x = layer(x)
+            tensors_by_layer[layer.name] = x
+        out_tensors = (
+            [tensors_by_layer[name] for name in style_names] +
+            [tensors_by_layer[content_name]]
         )
-        self.model = tf.keras.Model(inputs=vgg.input, outputs=outputs)
+        self.model = tf.keras.Model(inputs=vgg.input, outputs=out_tensors)
 
     def generate_features(self):
         """
         Extracts style Gram matrices and content layer activations.
 
-        Sets gram_style_features from the style image's style-layer outputs
-        and content_feature from the content image's content-layer output.
+        Sets style_features and gram_style_features from the style image's
+        style-layer outputs and content_feature from the content image's
+        content-layer output.
+        Images are scaled to [0, 1] then passed through VGG preprocessing
+        (ImageNet mean-centered BGR) before the network, matching VGG19.
         """
         n_style = len(self.style_layers)
-        style_outputs = self.model(self.style_image)
+        vgg19 = tf.keras.applications.vgg19
+        style_pre = vgg19.preprocess_input(self.style_image * 255.0)
+        content_pre = vgg19.preprocess_input(self.content_image * 255.0)
+        style_out = self.model(style_pre)
+        if not isinstance(style_out, (list, tuple)):
+            style_list = [style_out]
+        else:
+            style_list = list(style_out)
+        self.style_features = [style_list[i] for i in range(n_style)]
         self.gram_style_features = [
-            self.gram_matrix(style_outputs[i]) for i in range(n_style)
+            self.gram_matrix(style_list[i]) for i in range(n_style)
         ]
-        content_outputs = self.model(self.content_image)
-        self.content_feature = content_outputs[-1]
+        content_out = self.model(content_pre)
+        if not isinstance(content_out, (list, tuple)):
+            self.content_feature = content_out
+        else:
+            self.content_feature = content_out[-1]
 
     def layer_style_cost(self, style_output, gram_target):
         """
@@ -95,7 +129,7 @@ class NST:
         E_l = (1 / C_l^2) * sum_ij (G_ij - A_ij)^2
 
         Args:
-            style_output (tf.Tensor or tf.Variable): Layer activations (1,h,w,c).
+            style_output (tf.Tensor or tf.Variable): Activations (1, h, w, c).
             gram_target (tf.Tensor or tf.Variable): Target Gram (1, c, c).
 
         Returns:
@@ -113,18 +147,17 @@ class NST:
         else:
             c_dim = int(c_dim)
 
-        def _gram_target_typeerror():
-            return TypeError(
-                "gram_target must be a tensor of shape [1, {c}, {c}] where "
-                "{c} is the number of channels in style_output".format(c=c_dim)
-            )
+        gmsg = (
+            "gram_target must be a tensor of shape [1, {c}, {c}]".format(
+                c=c_dim)
+        )
 
         if not isinstance(gram_target, (tf.Tensor, tf.Variable)):
-            raise _gram_target_typeerror()
+            raise TypeError(gmsg)
 
         rank_g = gram_target.shape.ndims
         if rank_g is None or rank_g != 3:
-            raise _gram_target_typeerror()
+            raise TypeError(gmsg)
 
         g0 = gram_target.shape[0]
         g1 = gram_target.shape[1]
@@ -132,16 +165,17 @@ class NST:
         if g0 is not None and g1 is not None and g2 is not None:
             if (int(g0) != 1 or int(g1) != int(g2) or
                     int(g1) != c_dim):
-                raise _gram_target_typeerror()
+                raise TypeError(gmsg)
         else:
             gt = tf.shape(gram_target)
             if (int(gt[0].numpy()) != 1 or
                     int(gt[1].numpy()) != int(gt[2].numpy()) or
                     int(gt[1].numpy()) != c_dim):
-                raise _gram_target_typeerror()
+                raise TypeError(gmsg)
 
         gram_style = self.gram_matrix(style_output)
-        return tf.reduce_mean(tf.square(gram_style - gram_target))
+        gt = tf.cast(gram_target, gram_style.dtype)
+        return tf.reduce_mean(tf.square(gram_style - gt))
 
     def style_cost(self, style_outputs):
         """
@@ -160,8 +194,8 @@ class NST:
         if (not isinstance(style_outputs, list) or
                 len(style_outputs) != l_len):
             raise TypeError(
-                "style_outputs must be a list with a length of {l} where "
-                "{l} is the length of self.style_layers".format(l=l_len)
+                "style_outputs must be a list with a length of {n}".format(
+                    n=l_len)
             )
         weight = 1.0 / float(l_len)
         terms = []
@@ -191,8 +225,9 @@ class NST:
             raise TypeError("input_layer must be a tensor of rank 4")
 
         _, h, w, c = tf.unstack(tf.shape(input_layer))
-        feats = tf.reshape(input_layer, (1, h * w, c))
-        gram = tf.matmul(feats, feats, transpose_a=True)
+        features = tf.reshape(input_layer, (-1, c))
+        gram = tf.matmul(features, features, transpose_a=True)
+        gram = tf.expand_dims(gram, 0)
         gram = gram / tf.cast(h * w, tf.float32)
         return gram
 
